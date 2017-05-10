f10843c319a97d64c0c0a80a9c5d7250b694aa31
anonymframework <vahit.serif119@gmail.com>
2017-05-10 18:09:10 +0300

    made some updates

---
+++ src/Cable/Components/Facade.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: My
+ * Date: 05/10/2017
+ * Time: 18:07
+ */
+
+namespace Cable\Facade;
+/*
+ * This file belongs to the AnoynmFramework
+ *
+ * @author vahitserifsaglam <vahit.serif119@gmail.com>
+ * @see http://gemframework.com
+ *
+ * Thanks for using
+ */
+use Cable\Container\Container;
+
+/**
+ * Class Facade
+ * @package Anonym\Patterns
+ */
+class Facade
+{
+    /**
+     * the instance of container
+     *
+     * @var Container
+     */
+    protected static $container;
+    /**
+     * the resolved object instances
+     *
+     * @var array
+     */
+    protected static $resolvedInstance;
+    /**
+     * register the laravel container
+     *
+     * @param Container $container
+     */
+    public static function setApplication(Container $container){
+        static::$container = $container;
+    }
+    /**
+     * get the facade class
+     *
+     * @throws FacadeException
+     * @return string|Object
+     */
+    protected static function getFacadeClass()
+    {
+        throw new FacadeException('i can not call myself');
+    }
+    /**
+     * Resolve the facade root instance from the container.
+     *
+     * @param  string  $name
+     * @return mixed
+     */
+    protected static function resolveFacadeClass($name)
+    {
+        if (is_object($name)) {
+            return $name;
+        }
+        if (isset(static::$resolvedInstance[$name])) {
+            return static::$resolvedInstance[$name];
+        }
+        return static::$resolvedInstance[$name] = static::$container[$name];
+    }
+    /**
+     * call the method in registered instances
+     *
+     * @param string $method the name of method
+     * @param array $args the variables for method
+     * @return mixed
+     */
+    public static function __callStatic($method, $args = [])
+    {
+        $instance = static::resolveFacadeClass(static::getFacadeClass());
+        return call_user_func_array([$instance, $method], $args);
+    }
+}
